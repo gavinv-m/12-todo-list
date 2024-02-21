d540acca5eee7bcc9843cd8d327fe43776fe316a
Gavin <gavinvm@protonmail.com>
2024-02-21 13:51:55 +0200

    Validate task name and due date

--- src/index.js
+++ src/index.js
@@ -11,18 +11,28 @@ function addTaskId(arrayOfTasks) {
 
 function createNewTask() {
 
-    const taskName = prompt('Enter task name: ');
-    const day = prompt('Enter the due date in the following format: "YYYY-MM-DD": ');
-    // const taskDescription = prompt('Enter the description of the goal: '); 
+    let taskName = prompt('Enter task name: ');
+    while (!taskName) {
+        taskName = prompt('Enter task name: ');
+    }
+
+    let day = prompt('Enter the due date in the following format: "YYYY-MM-DD": ');
+    let dueDate = new Date(day);
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+
+    while (dueDate < today) {
+        day = prompt('Enter the due date in the following format: "YYYY-MM-DD": ');
+        dueDate = new Date(day);
+    }
+
+    const taskDescription = prompt('Enter the description of the goal: '); 
     const taskProject = prompt('Enter project name: '); 
-    // const priority = prompt('Enter priority level: ');
+    const priority = prompt('Enter priority level: ');
 
-    // const taskDetails = {taskName, day, taskDescription, taskProject, priority};
-
-    const taskDetails = {taskName, day, taskProject};
+    const taskDetails = {taskName, day, taskDescription, taskProject, priority};
 
     return taskDetails;
-
 }
 
 
