666501b4d7dbc31b4df52aaf9d531c4ae5081317
Gavin <gavinvm@protonmail.com>
2024-03-05 13:42:49 +0200

    Close form when cancel button clicked

--- src/button-handlers.js
+++ src/button-handlers.js
@@ -1,12 +1,28 @@
-const listenForAddTaskButton = () => {
-    const addTaskButton = document.querySelector('.add-task-btn');
-    const dialog = document.querySelector('.create-task-dialog');
+class TaskDialogHandler {
+    constructor(dialogClass) {
+        this.dialog = document.querySelector(dialogClass);
+    }
 
-    addTaskButton.addEventListener("click", () => {
-        dialog.showModal();
-    });
+    listenForAddTaskButton(addTaskBtnClass) {
+        const addTaskButton = document.querySelector(addTaskBtnClass);
+        addTaskButton.addEventListener("click", () => {
+            this.dialog.showModal();
+        });
+    }
+
+    listenForCancelFormButton(cancelFormBtnClass) {
+        const cancelFormButton = document.querySelector(cancelFormBtnClass);
+        cancelFormButton.addEventListener("click", (event) => {
+            event.preventDefault(); // Page was flickering
+            this.dialog.close();
+          });
+    }
 }
 
+
 export function setupClickEventListeners() {
-    listenForAddTaskButton();
+    const taskDialogHandler = new TaskDialogHandler('.create-task-dialog');
+
+    taskDialogHandler.listenForAddTaskButton('.add-task-btn');
+    taskDialogHandler.listenForCancelFormButton('.cancel-form-sub');    
 }
