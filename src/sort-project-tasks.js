06ef4ce289ff117218d5df4dc2058ed9a9e7cb36
Gavin <gavinvm@protonmail.com>
2024-03-18 22:31:29 +0200

    Display project tasks sorted by earliest

--- src/sort-project-tasks.js
+++ src/sort-project-tasks.js
@@ -6,6 +6,26 @@ function handleSortByDueDate(projectName) {
 
     // Get array of tasks in project
     const projectTasksSortedByEarliest = taskManager.sortProjectTasksByDate(projectName);
+
+    const handleSortByDueDate = new TaskDisplayHandler('.inner-container');
+
+    // Clear innerContainer
+    handleSortByDueDate.clearInnerContainer();
+    
+    for (let task of projectTasksSortedByEarliest) {
+
+        // For CSS try to maintain the same number of items in container as displayProjectTasks
+        // We are using the same class project-task
+        handleSortByDueDate.createTask('project-task', task); 
+        handleSortByDueDate.addTaskName();
+        handleSortByDueDate.addDueDate();
+        handleSortByDueDate.addDeleteButton(projectName); 
+
+        // NOTE: Different from other display functions
+        // Tasks are added directly to inner Container
+        // For styling make sure to use the selector class project-task
+        handleSortByDueDate.innerContainer.appendChild(handleSortByDueDate.task); 
+    }
 }
 
 
