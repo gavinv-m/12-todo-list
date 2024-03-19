89b96496adbe5e1dc8a35d7eb346633e97245754
Gavin <gavinvm@protonmail.com>
2024-03-20 00:28:27 +0200

    Sort tasks displayed in date groups

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './styles/style.css';
+import './styles/tasks.css';
 import { addImages } from './interface/add-images.js';
 import { setupClickEventListeners } from './interface/button-handlers.js';
 import { setUpDueDatesEventListeners } from './interface/due-dates-handlers.js';
